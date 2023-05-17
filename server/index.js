dfd5c0534ba35f5a424675ed4333cabeb37f786a
Lane Vance <lanevance92@gmail.com>
2023-05-17 11:05:45 -0600

    working up to 3.5

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
+const {sequelize} = require('./util/database')
+const {User} = require('./models/user')
+const {Post} = require('./models/post')
+
 const express = require('express')
 const cors = require('cors')
 
@@ -9,6 +13,9 @@ const {register, login} = require('./controllers/auth')
 const {isAuthenticated} = require('./middleware/isAuthenticated')
 
 const app = express()
+
+User.hasMany(Post)
+Post.belongsTo(User)
 
 app.use(express.json())
 app.use(cors())
@@ -23,4 +30,8 @@ app.post('/posts', isAuthenticated, addPost)
 app.put('/posts/:id', isAuthenticated, editPost)
 app.delete('/posts/:id', isAuthenticated, deletePost)
 
-app.listen(SERVER_PORT, () => console.log(`db sync successful & server running on port ${SERVER_PORT}`))
+sequelize.sync()
+.then(() => {
+    app.listen(PORT, () => console.log(`db sync successful & server running on port ${PORT}`))
+})
+.catch(err => console.log(theseHands))
